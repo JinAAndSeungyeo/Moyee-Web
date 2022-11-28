4ea06293cb501d0931fd639ae9c43b76649649b5
pjn141414 <jina071049@gmail.com>
2022-11-29 08:53:35 +0900

    Fix: ThemeProvider 설정 및 recoil 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app'
+import GlobalStyle from 'styles/globals';
+import theme from 'styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
